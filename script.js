9c4c1315e1b792a2208f850ed7afec31a508c9e0
Esmeralda-Freire <esmeralda.sfn@gmail.com>
2023-11-10 21:11:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function greeting() {
+    window.alert("Hello, welcome!");
+}
+
+greeting();
+
+const magicNumber = parseInt(prompt("Choose a number from 1 to 9 and see the message the universe has for you: "))
+
+function message(magicNumber) {
+    if(magicNumber >= 1 && magicNumber <= 5) {
+        let dayMsg = "Your day today will be ";
+        return dayMsg;
+    } else if(magicNumber > 5 && magicNumber <= 10) {
+        let dayMsg = "Your day tomorrow will be ";
+        return dayMsg;
+}
+}
+
+endMsg = () => {
+    if(magicNumber % 2 == 0) {
+        let endMsg = "very good, and an interesting surprise awaits you.";
+        return endMsg;
+    } else if(magicNumber % 2 != 0) {
+        let endMsg = "full of new experiences, allow yourself!";
+        return endMsg;
+    }
+}
+let magicMessage = message(magicNumber) + endMsg();
+
+window.alert(magicMessage);
